d6504c8686752215c8b2273bc7104c0354c8bee7
Johannes von Grundherr <74217475+Johannes-ece@users.noreply.github.com>
2023-04-22 15:31:32 +0200

    remove

--- tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '0ce5ff89420e3430c126827898c7f9fa975a9714', queries });
-export default client;
-  
